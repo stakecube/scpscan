857f412f640754969d4eb19b3c515ae0d9fec7fd
JSKitty <jskitty@protonmail.com>
2021-11-18 20:06:45 +0000

    [UI] Add "New" tag to tokens with no supply
    For most cases, this will handle and clarify when a new token has been created and does not yet have any supply.
    
    These conditions will also trigger for destroyed tokens (SCP-2 with fully burned supply), but it's not that important!

--- public/js/api.js
+++ public/js/api.js
@@ -159,7 +159,7 @@ $.getJSON('https://stakecubecoin.net/web3/scp/tokens/getalltokens', function(dat
       // Supply
       const tokenSupply = row.insertCell();
       const tokenSupplyPct = percentOf(nSupply, nMaxSupply).toFixed(isMobile ? 1 : 2) + '%';
-      tokenSupply.innerHTML = `${nHTML(nSupply, isMobile ? 2 : (nSupply > 100 ? 4 : 8))}<br style="margin-bottom: 5px;"><span style="cursor:pointer;margin-right:5px;${isMobile ? 'margin-bottom:5px;' : ''}" class="badge bg-info-new" title="${tokenSupplyPct} of the total ${formatName(strTicker, 6)} supply has been minted!">${tokenSupplyPct}</span>`;
+      tokenSupply.innerHTML = `${nHTML(nSupply, isMobile ? 2 : (nSupply > 100 ? 4 : 8))}<br style="margin-bottom: 5px;"><span style="cursor:pointer;margin-right:5px;${isMobile ? 'margin-bottom:5px;' : ''}" class="badge bg-info-new" title="${nSupply === 0 ? 'This token is new! The creator has not issued any supply yet.' : (tokenSupplyPct + ' of the total ' + formatName(strTicker, 6) + ' supply has been minted!')}">${nSupply === 0 ? 'New!' : tokenSupplyPct}</span>`;
       if (nVersion === 2 && data[i].APR > 0) {
         const tokenAPRStr = data[i].APR.toLocaleString('en-US', { maximumFractionDigits: isMobile ? 0 : (data[i].APR > 100 ? 0 : 2)}) + '%';
         tokenSupply.innerHTML += '<span style="cursor:pointer" class="badge bg-success-new" title="' + formatName(strTicker, 6) + ' rewards it\'s holders with ' + tokenAPRStr + ' APR">' + tokenAPRStr + ' APR</span>';
